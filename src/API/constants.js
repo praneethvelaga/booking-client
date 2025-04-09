e844f5b9c6c4858c628dc248d70fb96cb5c97fca
praneethvelaga <praneethvelaga1016@gmail.com>
2025-04-09 16:53:05 +0530

    Update API hostname for production

--- src/API/constants.js
+++ src/API/constants.js
@@ -1,6 +1,9 @@
 export default{
-    hostname: process.env.NODE_ENV === 'development' ? 'http://localhost:4000/api' : '/api',
+    hostname: process.env.NODE_ENV === 'development'
+  ? 'http://localhost:4000/api'
+  : 'https://booking-server.onrender.com/api',
 
+  
 publicEndPoints: new Map([
     ["auth", {path:"/auth/autontication", method: ['post']}],
     ["register", {path:"/regist/RegisterNewUser", method: ['post']}],
